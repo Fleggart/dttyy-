b3b26f4a3acd6b790ecca3912f7bbd0653f08723
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-08 00:40:00 +0800

    Add files via upload

--- src/main/java/com/dttyy/cropmod/item/ItemCornSeed.java
+++ src/main/java/com/dttyy/cropmod/item/ItemCornSeed.java
@@ -1,3 +1,9 @@
+package com.dttyy.cropmod.item;
+
+import net.minecraft.item.ItemSeeds;
+import net.minecraft.init.Blocks;
+import com.dttyy.cropmod.ModBlocks;
+
 public class ItemCornSeed extends ItemSeeds {
     public ItemCornSeed(Block crop) {
         super(crop, Blocks.FARMLAND);
