042ab91653b19319e97bafb6fecbe7f06c6c7bc1
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-07 11:42:20 +0800

    Add files via upload

---
+++ src/main/java/com/yourname/modinspector/ModMain.java
@@ -0,0 +1,17 @@
+package com.yourname.modinspector;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(
+    modid = ModInfo.MOD_ID,
+    name = ModInfo.NAME,
+    version = ModInfo.VERSION
+)
+public class ModMain {
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println(ModInfo.NAME + " initialized!");
+    }
+}
