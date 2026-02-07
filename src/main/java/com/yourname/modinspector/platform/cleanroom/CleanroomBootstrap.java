9d8a7398a7ea92498274868f6d607e3f3c7fc0ef
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-07 12:53:57 +0800

    Update CleanroomBootstrap.java

--- src/main/java/com/yourname/modinspector/platform/cleanroom/CleanroomBootstrap.java
+++ src/main/java/com/yourname/modinspector/platform/cleanroom/CleanroomBootstrap.java
@@ -3,9 +3,13 @@ package com.yourname.modinspector.platform.cleanroom;
 import com.yourname.modinspector.core.CoreLogic;
 
 public final class CleanroomBootstrap {
+
     public static void bootstrap() {
         System.out.println("CleanroomBootstrap running...");
         CoreLogic.run();
     }
-}
 
+    public static void main(String[] args) {
+        bootstrap(); // 可以直接运行测试
+    }
+}
