26003e2f58883bd07c4e94206061513fb71d185d
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-07 10:50:13 +0800

    Update ModMain.java

--- src/main/java/com/yourname/modinspector/ModMain.java
+++ src/main/java/com/yourname/modinspector/ModMain.java
@@ -1,6 +1,6 @@
 package com.yourname.modinspector;
 
-import com.cleanroom.loader.api.Mod; // 假设 Cleanroom Loader 提供的注解
+import com.cleanroom.mod.api.Mod; // 假设 Cleanroom mod 注解
 
 @Mod(
     id = ModInfo.MOD_ID,
@@ -10,7 +10,7 @@ import com.cleanroom.loader.api.Mod; // 假设 Cleanroom Loader 提供的注解
 public class ModMain {
 
     public void onInitialize() {
-        System.out.println(ModInfo.NAME + " initialized on Cleanroom Loader!");
+        System.out.println(ModInfo.NAME + " initialized as a Cleanroom Mod!");
         platform.cleanroom.CleanroomBootstrap.bootstrap();
     }
 }
