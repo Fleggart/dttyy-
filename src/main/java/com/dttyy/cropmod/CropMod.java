c4a431e85459a4a58a26a1e02830dc3b87b5f90f
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-09 00:39:17 +0800

    Update CropMod.java

--- src/main/java/com/dttyy/cropmod/CropMod.java
+++ src/main/java/com/dttyy/cropmod/CropMod.java
@@ -5,14 +5,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = CropMod.MODID, name = CropMod.NAME, version = CropMod.VERSION)
 public class CropMod {
-
     public static final String MODID = "cropmod";
     public static final String NAME = "Crop Mod";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0";
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        // 不需要手动注册 Block/Item，
-        // Forge EventBusSubscriber 已完成注册
+        // 注册逻辑已经在 ModBlocks/ModItems 的事件里
     }
 }
