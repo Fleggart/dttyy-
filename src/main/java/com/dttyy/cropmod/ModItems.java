f2a5667ffeec9e5cf93d778ac4910156a97d223c
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-10 07:23:09 +0800

    Update ModItems.java

--- src/main/java/com/dttyy/cropmod/ModItems.java
+++ src/main/java/com/dttyy/cropmod/ModItems.java
@@ -7,43 +7,53 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import com.dttyy.cropmod.block.BlockCropBase;
-import com.dttyy.cropmod.item.ItemCornSeed;
+import com.dttyy.cropmod.item.CropSeedItem;
+import com.dttyy.cropmod.item.CropFoodItem;
+import com.dttyy.cropmod.item.CropMaterialItem;
+
 import java.util.HashMap;
 import java.util.Map;
 
 @Mod.EventBusSubscriber(modid = CropMod.MODID)
 public class ModItems {
+
     public static final Map<String, Item> SEEDS = new HashMap<>();
     public static final Map<String, ItemFood> FOODS = new HashMap<>();
-    public static final Map<String, Item> NORMALS = new HashMap<>();
+    public static final Map<String, Item> MATERIALS = new HashMap<>();
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
+
         for (CropType type : CropType.values()) {
+
             BlockCropBase cropBlock = ModBlocks.CROPS.get(type.getName());
 
             // 种子
-            Item seed = new ItemCornSeed(cropBlock)
+            Item seed = new CropSeedItem(cropBlock)
                 .setRegistryName(CropMod.MODID, type.getName() + "_seed")
                 .setTranslationKey(CropMod.MODID + "." + type.getName() + "_seed");
             SEEDS.put(type.getName(), seed);
             event.getRegistry().register(seed);
 
-            // 可食用果实
-            ItemFood food = new ItemFood(type.getHunger(), type.getSaturation(), false);
-            food.setRegistryName(CropMod.MODID, type.getName() + "_food");
-            food.setTranslationKey(CropMod.MODID + "." + type.getName() + "_food");
-            FOODS.put(type.getName(), food);
-            event.getRegistry().register(food);
+            // 食物型果实
+            if (type.hasFood()) {
+                ItemFood foodItem = new CropFoodItem(type.getName(), type.getName() + "_food")
+                    .setRegistryName(CropMod.MODID, type.getName() + "_food")
+                    .setTranslationKey(CropMod.MODID + "." + type.getName() + "_food");
+                FOODS.put(type.getName(), foodItem);
+                event.getRegistry().register(foodItem);
+            }
 
-            // 普通果实（材料/工具用途）
-            Item normal = new Item()
-                .setRegistryName(CropMod.MODID, type.getName() + "_item")
-                .setTranslationKey(CropMod.MODID + "." + type.getName() + "_item");
-            NORMALS.put(type.getName(), normal);
-            event.getRegistry().register(normal);
+            // 普通材料类型物品
+            if (type.hasMaterial()) {
+                Item materialItem = new CropMaterialItem(type.getName() + "_item")
+                    .setRegistryName(CropMod.MODID, type.getName() + "_item")
+                    .setTranslationKey(CropMod.MODID + "." + type.getName() + "_item");
+                MATERIALS.put(type.getName(), materialItem);
+                event.getRegistry().register(materialItem);
+            }
 
-            // 注册 ItemBlock 使作物方块显示正常
+            // 注册方块对应的 ItemBlock
             event.getRegistry().register(new ItemBlock(cropBlock)
                 .setRegistryName(cropBlock.getRegistryName()));
         }
