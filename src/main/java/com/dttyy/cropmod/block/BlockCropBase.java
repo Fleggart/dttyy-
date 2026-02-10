5ca4a572327ba70b3f97e87319ef4064746758c6
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-11 00:44:48 +0800

    Update BlockCropBase.java

--- src/main/java/com/dttyy/cropmod/block/BlockCropBase.java
+++ src/main/java/com/dttyy/cropmod/block/BlockCropBase.java
@@ -1,10 +1,10 @@
 package com.dttyy.cropmod.block;
 
+import com.dttyy.cropmod.ModItems;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
-import com.dttyy.cropmod.ModItems;
 
 public class BlockCropBase extends BlockCrops {
 
@@ -30,9 +30,7 @@ public class BlockCropBase extends BlockCrops {
     public void getDrops(NonNullList<ItemStack> drops, net.minecraft.world.IBlockAccess world,
                          net.minecraft.util.math.BlockPos pos,
                          net.minecraft.block.state.IBlockState state, int fortune) {
-
         super.getDrops(drops, world, pos, state, fortune);
-
         if (ModItems.MATERIALS.containsKey(cropName)) {
             drops.add(new ItemStack(ModItems.MATERIALS.get(cropName)));
         }
