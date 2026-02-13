f7a1a02ffce12d7d531206cb0ec57ecea57caa02
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-14 05:31:30 +0800

    Create CropFoodItem.java

---
+++ src/main/java/com/dttyy/cropmod/crop/CropFoodItem.java
@@ -0,0 +1,5 @@
+package com.dttyy.cropmod.crop;
+
+import net.minecraft.item.ItemFood;
+
+public class CropFoodItem extends ItemFood { public CropFoodItem(int hunger, float saturation) { super(hunger, saturation, false); } }
