04dd9d40733c36fe9f294251308ba557d08813da
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-14 06:28:33 +0800

    Create BlockStemGeneric.java

---
+++ src/main/java/com/dttyy/cropmod/block/BlockStemGeneric.java
@@ -0,0 +1,11 @@
+package com.dttyy.cropmod.block;
+
+import net.minecraft.block.BlockStem;
+import net.minecraft.block.Block;
+
+public class BlockStemGeneric extends BlockStem {
+
+    public BlockStemGeneric(Block fruit) {
+        super(fruit);
+    }
+}
