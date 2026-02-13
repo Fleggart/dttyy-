a0ea53950718f67e58c5250ad4baae63f46e36f6
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-14 05:26:28 +0800

    Update CropType.java

--- src/main/java/com/dttyy/cropmod/data/CropType.java
+++ src/main/java/com/dttyy/cropmod/data/CropType.java
@@ -2,8 +2,10 @@ package com.dttyy.cropmod.data;
 
 public enum CropType {
 
-ASPARAGUS("asparagus", 3, 0.6f, false),
-RYE("rye", 3, 0.6f, false);
+    Oat("Oat", 3, 0.6f, false),
+    rye("rye", 3, 0.6f, false),
+    Corn("Corn", 3, 0.6f, false),
+    Barley("Barley", 3, 0.6f, false);
 
 private final String name;
 private final int hunger;
