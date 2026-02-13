856fb70cf3bc397ad27550619bd043aa6e5dc44c
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-14 05:25:28 +0800

    Create CropType.java

---
+++ src/main/java/com/dttyy/cropmod/data/CropType.java
@@ -0,0 +1,25 @@
+package com.dttyy.cropmod.data;
+
+public enum CropType {
+
+ASPARAGUS("asparagus", 3, 0.6f, false),
+RYE("rye", 3, 0.6f, false);
+
+private final String name;
+private final int hunger;
+private final float saturation;
+private final boolean hasMaterial;
+
+CropType(String name, int hunger, float saturation, boolean hasMaterial) {
+    this.name = name;
+    this.hunger = hunger;
+    this.saturation = saturation;
+    this.hasMaterial = hasMaterial;
+}
+
+public String getName() { return name; }
+public int getHunger() { return hunger; }
+public float getSaturation() { return saturation; }
+public boolean hasMaterial() { return hasMaterial; }
+
+}
